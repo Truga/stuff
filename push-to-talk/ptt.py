34a37a6fa4637c85d3ffdaee97717ded60000310
truga <truga@no>
2026-01-17 12:26:07 +0100

    use alsa, can be simply ran as root

--- push-to-talk/ptt.py
+++ push-to-talk/ptt.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
-# you must have read permission on the /dev/input device to be able to do this
-# this is probably a really bad idea on a keyboard
 # conf
-PTTKey='F20'
+# to get a list of valid buttons, run evtest and select your device
+# to find the name of your mic, run arecord -l
+# to find your microphone toggle option, run amixer -c <mic> controls
+# chuck ptt.py into /usr/local/bin/
+# chuck ptt.service into /etc/systemd/system/
+# systemctl --enable ptt.service
+
+PTTKey='KEY_STOP'
 PTTSource='/dev/input/by-id/usb-Logitech_Gaming_Mouse_G600_342192E245030017-if01-event-kbd'
-PTTMic='alsa_input.usb-Antlion_Audio_Antlion_Wireless_Microphone-00.mono-fallback'
+PTTMic='Microphone'
+PTTMicToggle="'Mic Capture Switch'"
+
 # no conf below here
 
 import subprocess
@@ -12,9 +19,10 @@ from evdev import InputDevice, categorize, ecodes
 dev = InputDevice(PTTSource)
 
 for event in dev.read_loop():
-    if event.code == ecodes.ecodes['KEY_'+PTTKey]:
+    # print(categorize(event))
+    if event.code == ecodes.ecodes[PTTKey]:
         match event.value:
             case 0:
-                subprocess.run(['/usr/bin/pactl', 'set-source-mute', PTTMic, '1'])
+                subprocess.run(['/usr/bin/amixer', '-qc', PTTMic, 'cset', 'name=' + PTTMicToggle, '0'])
             case 1:
-                subprocess.run(['/usr/bin/pactl', 'set-source-mute', PTTMic, '0'])
+                subprocess.run(['/usr/bin/amixer', '-qc', PTTMic, 'cset', 'name=' + PTTMicToggle, '1'])
